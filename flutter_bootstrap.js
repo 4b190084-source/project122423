272a0ec499b734380bdb922c17c69da5d3d590cb
4b190084-source <4b190084@stust.edu.tw>
2025-12-25 08:39:03 +0800

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1943956309"
+    serviceWorkerVersion: "3495665289"
   }
 });
